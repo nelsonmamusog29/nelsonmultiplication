390faac688401c6356e39406374d374397af3b92
nelsonmamusog29 <59050050+nelsonmamusog29@users.noreply.github.com>
2019-12-22 21:07:37 +0800

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MEDION
+ */
+public class Multiplication {
+    public static void main(String []args){
+//        Scanner in =new Scanner(System.in);
+//        System.out.print("Enter a number from: ");
+//       int numFrom=in.nextInt();
+//        System.out.print("Enter a number to: ");
+    //   int numTo=in.nextInt();
+        for (int i = 0; i <10; i++) {
+            for (int j = 1; j <10 ; j++) {
+                   System.out.print(i +"X"+ j+" = " +i * j+"\t      ");
+                   
+            }
+            System.out.println("");
+   
+            
+        }
+    }
+}
+
+
+
